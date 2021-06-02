b30d090bfacdcbe1ee9439e29f25d73b4ec99f35
Shawn Zheng <shawn@yuansfer.com>
2021-06-02 17:16:14 -0400

    bug

--- Test/tests/common/InitYuanpayConfig.cs
+++ Test/tests/common/InitYuanpayConfig.cs
@@ -10,12 +10,7 @@ namespace Yuansfer_SDK.tests.common
         {
             YuanpayConfig config = new YuanpayConfig();
             config.env = EnvironmentEnums.SANDBOX.Value;
-            //config.merchantNo = "201170";
-            //config.storeNo = "301315";
-            //config.token = "247dba927a664c7e8b1a8c165cda219e";
-            //config.merchantNo = "200793";
-            //config.storeNo = "301068";
-            //config.token = "4c44c0ebb2597c572c225a4b65442ee7";
+            
             config.merchantNo = "202333 ";
             config.storeNo = "301854";
             config.token = "17cfc0170ef1c017b4a929d233d6e65e";
