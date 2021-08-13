177685e95edc8e28087b3242e1a57cf6a20b29db
Shawn Zheng <shawn@yuansfer.com>
2021-08-13 16:54:45 -0400

    Add new Generate Mixed QRC API

---
+++ src/response/offline/GenerateMixedQrcodeResponse.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace Yuansfer_SDK.src.response.offline
+{
+    [DataContract]
+    public class GenerateMixedQrcodeResponse : YuanpayResponse
+    {
+        [DataMember]
+        public JObject result { get; set; }
+    }
+}
